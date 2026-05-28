39d180759b148ef50da7912fa1a3720861b0d68b
John Doe <madda.athiarahman@gmail.com>
2026-05-28 13:56:44 +0700

    setup configuration for deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // API to connect to Express backend
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 
 export const generateProducts = async (wasteType) => {
